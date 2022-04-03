f9afad112c4e92f73299d4b4b66be53c62060072
SisolusMaximus <jaimperator99@gmail.com>
2022-04-03 16:49:15 +0200

    Crated FormControl for Register for and bound it with the template

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+
+  public registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3) 
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl('')
+  })
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
