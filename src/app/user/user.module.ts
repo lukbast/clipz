a144a767426be5c8fbcf3e33dc6e82d1a52a6567
SisolusMaximus <jaimperator99@gmail.com>
2022-04-03 09:53:20 +0200

    changed Modal so it's appended at the end of the body of the document to avoid CSS issues. Fixed text color.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [
     AuthModalComponent
